0c3e000fce30762d47a85b8c5f4fb3e52704fd90
jayden <jayden@oath.com>
2019-01-10 02:04:55 +0800

    add 2.3

--- src/linked_list/NO_2_3.java
+++ src/linked_list/NO_2_3.java
@@ -1,4 +1,20 @@
 package linked_list;
 
+import util.ListNode;
+/*
+題目：刪除某個節點
+知識點：
+由於沒有給節點的頭，就是複製下一個節點，直接連過去即可
+ */
 public class NO_2_3 {
+    boolean deleteNode(ListNode n){
+        if(n==null || n.next==null) {
+            return false;
+        }
+        ListNode nextCopy = n.next;
+        n.data = nextCopy.data;
+        n.next = nextCopy.next;
+
+        return true;
+    }
 }
